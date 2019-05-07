17d91deb1b1aedbdb228684a2577fa93b3ab6f69
Tyler Hunt <tyhunt04@gmail.com>
2019-05-07 16:55:24 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -29,7 +29,7 @@ class Boat < ActiveRecord::Base
 
   def self.with_three_classifications
     includes(:classifications).find_each do |classification|
-      classification.where(classification.count == 3)
+      classification.where(classifications.count == 3)
     end
   end
 end
